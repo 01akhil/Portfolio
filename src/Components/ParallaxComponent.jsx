e54132d28ca94dc81cc58563b34a3b062974c4f1
Akhilesh Bhadula <41585465+01akhil@users.noreply.github.com>
2024-07-22 18:16:26 +0530

    Update ParallaxComponent.jsx

--- src/Components/ParallaxComponent.jsx
+++ src/Components/ParallaxComponent.jsx
@@ -26,14 +26,14 @@ const ParallaxComponent = () => {
   return (
     <main className="w-screen h-screen overflow-hidden relative">
       <img
-        src="./src/WhatsApp Image 2024-07-21 at 22.59.10_40b447af.jpg"
+        src=".src/WhatsApp Image 2024-07-21 at 22.59.10_40b447af.jpg"
         data-speedx="0.065"
         data-speedy="0.04"
         className="parallax absolute top-1/2 left-1/2 w-full h-auto min-w-[200vw] transform -translate-x-1/2 -translate-y-1/2 transition-transform duration-500 ease-[cubic-bezier(0.2,0.49,0.32,0.99)] pointer-events-none"
         alt="Background Image"
       />
       <img
-        src="./src/arock_landing_page_model-removebg.jpg"
+        src=".src/arock_landing_page_model-removebg.jpg"
         data-speedx="0.085"
         data-speedy="0.05"
         className="parallax absolute top-1/2 left-1/2 w-full h-auto transform -translate-x-1/2 -translate-y-1/2 transition-transform duration-500 ease-[cubic-bezier(0.2,0.49,0.32,0.99)] pointer-events-none"
